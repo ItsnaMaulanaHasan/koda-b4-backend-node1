9d1cd31ff5d611fec59caf604f9aeaf4c620f90e
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-25 18:48:35 +0700

    feat: add validation file size

--- src/lib/upload.js
+++ src/lib/upload.js
@@ -14,6 +14,9 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1 * 1024 * 1024 },
+});
 
 module.exports = upload;
