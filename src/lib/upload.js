803178f2c0e653def775dbd94210f14ce946a317
ItsnaMaulanaHasan <hasanmaulana453@gmail.com>
2025-11-25 18:08:02 +0700

    feat: create lib for upload image

---
+++ src/lib/upload.js
@@ -0,0 +1,19 @@
+const multer = require("multer");
+const path = require("node:path");
+const process = require("node:process");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    const ext =
+      file.originalname.split(".")[file.originalname.split(".").length - 1];
+    cb(null, file.originalname.split(".")[0] + "-" + uniqueSuffix + "." + ext);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
